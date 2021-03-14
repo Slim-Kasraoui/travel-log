6f22497a1fde66173d413122e7822e43565b59ca
slim.kasraoui2020@gmail.com <slim.kasraoui2020@gmail.com>
2021-03-14 03:08:30 +0100

    entryModel/get/post done

--- server/src/index.js
+++ server/src/index.js
@@ -2,24 +2,38 @@ const express = require("express")
 const morgan = require("morgan")
 const helmet = require("helmet")
 const cors = require("cors")
+const mongoose = require("mongoose")
 
-const middlewares = require('./middlwares')
+require("dotenv").config()
+
+const middlewares = require("./middlwares")
+const logs = require('./api/logs')
 
 const app = express()
+console.log(process.env.DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
 app.use(morgan("common"))
 app.use(helmet())
-app.use(cors({
-  origin: "http://localhost:3000"
-}))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  }),
+)
+app.use(express.json())
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send({
-    message: "hello world!"
+    message: "hello world!",
   })
 })
 
-app.use(middlewares.notFound)
 
+app.use('/api/logs', logs)
+app.use(middlewares.notFound)
 app.use(middlewares.errorHandler)
 
 const port = process.env.PORT || 3000
--- server/src/middlwares.js
+++ server/src/middlwares.js
@@ -9,8 +9,8 @@ module.exports = {
     next(error)
   },
   errorHandler : (error, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
-    res.status = statusCode
+    const statusCode = res.statusCode === 200 ? 418 : res.statusCode
+    res.status(statusCode)
     res.json({
       message : error.message,
       stack: process.env.NODE_ENV === "production" ? "pancake" : error.stack
